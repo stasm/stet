0094db1db97b6fa8d99c27aaaaf1f4599cde39f1
Stas Malolepszy <stas@mozilla.com>
2011-05-26 23:41:42 -0700

    Add a nicer admin layout for Article

--- stet/admin.py
+++ stet/admin.py
@@ -3,6 +3,20 @@ from django.contrib import admin
 from stet.models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display_links = ('title',)
+    list_display = ('id', 'title', '_status', 'pub_date')
+    fieldsets = (
+        (None, {
+            'fields': ('title', '_status', 'body')
+        }),
+        ('Meta', {
+            'classes': ('collapse',),
+            'fields': ('tags', 'excerpt'),
+        }),
+        ('Under the hood', {
+            'classes': ('collapse',),
+            'fields': ('pub_date', 'filename', 'html'),
+        }),
+    )
 
 admin.site.register(Article, ArticleAdmin)
